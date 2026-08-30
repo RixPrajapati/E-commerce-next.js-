c6c5d33ce8205368315049f36e0454a8a2ef168d
Rixbroho <ratnaprajapati10@gmail.com>
2026-08-30 23:48:43 +0545

    iaj

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
   return (
     <div className='text-center'>
         <h1 className='text-3xl text-red-700 font-bold'>404</h1>
-        <h3>Page not found</h3>
+        <h3>Page not found </h3>
     </div>
   )
 }
