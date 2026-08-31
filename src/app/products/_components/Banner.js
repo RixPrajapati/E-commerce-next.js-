836162be65a0b38f38d49e5db1df4647fd3a4587
Rixbroho <ratnaprajapati10@gmail.com>
2026-08-31 23:47:28 +0545

    making layout of product route (banner)

--- src/app/products/_components/Banner.js
+++ src/app/products/_components/Banner.js
@@ -4,10 +4,12 @@ import watchImage from "@/assets/images/rolex.png"
 
 const ProductBanner = () => {
   return (
-    <div className='p-10 bg-linear-to-r from-blue-800 via-blue-300 to-blue-700 shadow-xl grid grid-col-3 items-center justify-between'>
-        <h2 className='font-black text-white text-7xl'>New Arrival</h2>
-        <Image src={watchImage} height={400} width={400} className='h-64 auto'/>
-        <div>50% Off</div>
+    <div className='p-10 bg-linear-to-r from-blue-800 via-blue-300 to-blue-700 shadow-xl grid grid-cols-3 items-center '>
+        <h2 className='font-black text-white text-7xl text-center'>New Arrival</h2>
+        <Image src={watchImage} height={400} width={400} className='h-64 w-auto mx-auto'/>
+        <h4 className='flex flex-col items-center justify-center rounded-full bg-white shadow border-4 border-blue-200 w-40 h-40 font-bold text-2xl text-center'>
+            <span className='text-5xl text-red-600'>50%</span> Off
+        </h4>
     </div>
   )
 }
