021e4952f9b0286683eb2ff193f83ebce7f59d1e
Rixbroho <ratnaprajapati10@gmail.com>
2026-09-01 00:13:46 +0545

    adding error.js in products

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
-const ProductDetail = async ({params}) => {
-    const {id}= await params
-  return (
-    <div>Product Detail by id : {id}</div>
-  )
-}
+const ProductDetail = async ({ params }) => {
+  const { id } = await params;
+  try {
+    const products = await fetch(
+      `https://mern-20260320-api.vercel.app/api/products/${id}`,
+    ).then((res) => res.json());
+    console.log(products);
+  } catch (err) {
+    throw err;
+  }
 
-export default ProductDetail
+  return <div>Product Detail by id : {id}</div>;
+};
+
+export default ProductDetail;
