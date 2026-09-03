4139fb7e52c1b13390da9f37de3488f485705017
Rixbroho <ratnaprajapati10@gmail.com>
2026-09-04 00:45:07 +0545

    made card

--- src/app/products/_components/Card.js
+++ src/app/products/_components/Card.js
@@ -1,7 +1,42 @@
+import { PRODUCTS_ROUTE } from "@/constants/routes";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-const ProductsCard = () => {
-  return <div>ProductsCard</div>;
+const ProductsCard = ({ name, brand, category, price, _id, imageUrls }) => {
+  return (
+    <div className="product-card">
+      <Image
+        className="w-full h-40 object-cover"
+        src={imageUrls[0]}
+        alt={`${name} image`}
+        width={400}
+        height={300}
+      />
+      <div className="bg-white dark:bg-gray-900 pt-3 px-4 pb-4">
+        <span className="bg-accent text-white text-xs font-medium px-1.5 py-0.5 rounded">
+          {category}
+        </span>
+        <Link href={`${PRODUCTS_ROUTE}/${_id}`}>
+          <h4 className="dark:text-white font-semibold text-xl pt-1 pb-2">
+            {name}
+          </h4>
+        </Link>
+        <p>
+          Brand: <strong>{brand}</strong>
+        </p>
+        <p className="my-1 font-bold text-2xl text-primary">{price}</p>
+        <div className="grid grid-cols-[auto_1fr] justify-between items-center gap-4">
+          <Link href={`${PRODUCTS_ROUTE}/${_id}`} className="px-10 bg-background dark:bg-gray-800 py-2 w-full text-center rounded-3xl mt-2 text-sm font-medium transition duration-300 ease hover:text-primary dark:text-gray-300">
+            View
+          </Link>
+          <button className="bg-primary px-4 py-2 w-full text-center rounded-3xl mt-2 text-sm font-medium transition duration-300 ease text-white">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductsCard;
--- src/app/products/layout.js
+++ src/app/products/layout.js
@@ -4,8 +4,11 @@ import ProductBanner from "./_components/Banner";
 const ProductLayout = ({ children }) => {
   return (
     <div className="container mx-auto px-4 py-16">
-      <ProductBanner/>
-      <section className="py-16">{children}</section>
+      <ProductBanner />
+      <section className="py-16">
+        
+        {children}
+      </section>
     </div>
   );
 };
