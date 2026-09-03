8692fc765fc894eb5321121061d3906ec6cfe75e
Rixbroho <ratnaprajapati10@gmail.com>
2026-09-03 23:57:17 +0545

    adding light theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata={
 const RootLayout = ({children}) => {
   return (
     <html lang='en'>
-      <body>
+      <body className='light'>
         <Header/>
         {children}
       </body>
