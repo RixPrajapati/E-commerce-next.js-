95d334e753ebfcdd5a62547f2d57471193f337c2
Rixbroho <ratnaprajapati10@gmail.com>
2026-09-12 23:34:49 +0545

    image can be see in add proudct page

--- src/app/(private)/(merchant)/product-management/_components/Form.js
+++ src/app/(private)/(merchant)/product-management/_components/Form.js
@@ -1,15 +1,18 @@
-'use client'
+"use client";
 
+import Image from "next/image";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaCloudArrowUp } from "react-icons/fa6";
 
 const ProductForm = () => {
+  const { register, handleSubmit } = useForm();
+  const [productImages, setProductImages] = useState([]);
+  const [localImageUrls, setLocalImageUrls] = useState([]);
 
-    const {register,handleSubmit}=useForm();
-
-    function formSubmit(data){
-        console.log(data)
-    }
+  function formSubmit(data) {
+    console.log(data);
+  }
   return (
     <form onSubmit={handleSubmit(formSubmit)}>
       <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
@@ -18,7 +21,7 @@ const ProductForm = () => {
             htmlFor="name"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Product Name
+            Product Name *
           </label>
           <input
             type="text"
@@ -26,7 +29,7 @@ const ProductForm = () => {
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
             placeholder="Type product name"
             required
-           {...register("name")}
+            {...register("name")}
           />
         </div>
         <div className="w-full">
@@ -34,7 +37,7 @@ const ProductForm = () => {
             htmlFor="brand"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Brand
+            Brand *
           </label>
           <input
             type="text"
@@ -50,7 +53,7 @@ const ProductForm = () => {
             htmlFor="price"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Price
+            Price *
           </label>
           <input
             type="number"
@@ -66,7 +69,7 @@ const ProductForm = () => {
             htmlFor="category"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Category
+            Category *
           </label>
           <input
             type="text"
@@ -82,7 +85,7 @@ const ProductForm = () => {
             htmlFor="stock"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Stock
+            Stock *
           </label>
           <input
             type="number"
@@ -94,21 +97,63 @@ const ProductForm = () => {
           />
         </div>
 
-        <div className="sm:col-span-2 flex items-center justify-center w-full">
+        <div className="sm:col-span-2">
           <label
-            htmlFor="dropzone-file"
-            className="text-gray-500 flex flex-col items-center justify-center w-full h-64 bg-neutral-secondary-medium border border-dashed border-default-strong rounded cursor-pointer hover:bg-neutral-tertiary-medium"
+            htmlFor="images"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            <div className="flex flex-col items-center justify-center text-body pt-5 pb-6">
-              <FaCloudArrowUp className="w-8 h-8 mb-4"/>
-              <p className="mb-2 text-sm">
-                <span className="font-semibold">Click to upload</span> or drag
-                and drop
-              </p>
-              <p className="text-xs">PNG, JPG or WEBP</p>
-            </div>
-            <input id="dropzone-file" type="file" className="hidden" />
+            Product Image *
           </label>
+          <div className="sm:col-span-2 flex items-center justify-center w-full">
+            <label
+              htmlFor="images"
+              className="text-gray-500 flex flex-col items-center justify-center w-full h-64 bg-neutral-secondary-medium border border-dashed border-default-strong rounded cursor-pointer hover:bg-neutral-tertiary-medium"
+            >
+              <div className="flex flex-col items-center justify-center text-body pt-5 pb-6">
+                <FaCloudArrowUp className="w-8 h-8 mb-4" />
+                <p className="mb-2 text-sm">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="text-xs">PNG, JPG or WEBP</p>
+              </div>
+              <input
+                id="images"
+                type="file"
+                className="hidden"
+                multiple
+                accept=".png,.jpg,.jpeg, .webp"
+                onChange={(event) => {
+                  const files = [];
+                  const imageUrl = [];
+                  Array.from(event.target.files).map((file) => {
+                    files.push(file);
+                    imageUrl.push(URL.createObjectURL(file));
+                  });
+                  setProductImages(files);
+                  setLocalImageUrls(imageUrl);
+                }}
+              />
+            </label>
+          </div>
+          {localImageUrls.length > 0 && (
+            <div className="flex py-4 gap-2">
+              {localImageUrls.map((imageUrl, index) => (
+                <div
+                  key={index}
+                  className="p-0 5 border rounded-lg border-gray-200 dark:border-gray-700"
+                >
+                  <Image
+                    src={imageUrl}
+                    alt=""
+                    height={64}
+                    width={64}
+                    className="h-16 w-16 object-cover rounded"
+                  />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
 
         <div className="sm:col-span-2">
